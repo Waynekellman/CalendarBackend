29554ae650efbacf8a2387d24d8a63857133e1c1
Wayne Kellman <waynekellman@ac.c4q.nyc>
2018-06-21 19:50:06 -0400

    switched to mysql2 db

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2018_06_21_222020) do
 
-  create_table "events", force: :cascade do |t|
+  create_table "events", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.string "month"
